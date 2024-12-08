4850eff000d807306f0dc40a3ffad9012bc0b834
Rithika P <166213945+Rithika2406@users.noreply.github.com>
2024-12-08 14:07:22 +0530

    Add files via upload

---
+++ twicethenum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int t;
+    int N;
+    int i = 1;
+    scanf("%d", &t);
+    while (i <= t) 
+    {
+        scanf("%d", &N);
+        printf("%d\n", 2 * N);
+        i = i+1;
+    }
+  
+}
